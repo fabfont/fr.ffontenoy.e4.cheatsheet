abc5843c5d8ae1dafc8ab42bbf9073b1e0795d27
fab <fabrice.fontenoy@gmail.com>
2015-09-07 23:08:07 +0200

    Add the possibility to change the shortcut

--- src/fr/ffontenoy/e4/cheatsheet/processors/CheatsheetProcessor.java
+++ src/fr/ffontenoy/e4/cheatsheet/processors/CheatsheetProcessor.java
@@ -3,6 +3,7 @@ package fr.ffontenoy.e4.cheatsheet.processors;
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.List;
 import java.util.Locale;
 
 import javax.inject.Named;
@@ -12,11 +13,17 @@ import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.core.services.translation.TranslationService;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.commands.MBindingTable;
+import org.eclipse.e4.ui.model.application.commands.MKeyBinding;
+import org.eclipse.e4.ui.model.application.ui.MUIElement;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
 
 import fr.ffontenoy.e4.cheatsheet.CheatsheetActivator;
 import fr.ffontenoy.e4.cheatsheet.extensionpoint.CheatsheetExtensionAttribute;
 import fr.ffontenoy.e4.cheatsheet.parts.CheatsheetsPart;
 import fr.ffontenoy.e4.cheatsheet.utils.XMLParserForCheatSheet;
+import fr.ffontenoy.e4.stickyview.StickyViewConstants;
 
 /**
  * Cheat sheet Processor
@@ -26,7 +33,12 @@ import fr.ffontenoy.e4.cheatsheet.utils.XMLParserForCheatSheet;
  */
 public class CheatsheetProcessor {
 
-  /***
+  /**
+   * Binding table id for cheatsheet
+   */
+  private static final String CHEATSHEET_BINDING_TABLE_ID = "fr.ffontenoy.e4.cheatsheet.CheatsheetBindingTable";
+
+  /**
    * Cheatsheet extension point id
    */
   public static final String CHEATSHEET_EXTENSION_POINT_ID = "fr.ffontenoy.e4.cheatsheet.cheatsheetExtensionPoint";
@@ -37,7 +49,7 @@ public class CheatsheetProcessor {
   private static final String CHEATSHEET_FILE_EXTENSION = ".xml";
   
   @Execute
-  public void process(IExtensionRegistry pExtensionRegistry) {
+  public void process(IExtensionRegistry pExtensionRegistry, EModelService pModelService, MApplication pApplication) {
     IConfigurationElement[] lConfigurationElements =
         pExtensionRegistry.getConfigurationElementsFor(CHEATSHEET_EXTENSION_POINT_ID);
 
@@ -50,7 +62,8 @@ public class CheatsheetProcessor {
       String lCheatsheetFile = lConfigurationElement.getAttribute(CheatsheetExtensionAttribute.CHEATSHEET_FILE.getName());
 
       String lShortcut = lConfigurationElement.getAttribute(CheatsheetExtensionAttribute.SHORTCUT.getName());
-
+      setCheatsheetShortcut(pApplication, lShortcut);
+      
       String lBundleId = lConfigurationElement.getContributor().getName();
 
       URL lInternationalizedCheatsheetURL = getInternationalizedCheatsheetFile(lCheatsheetFile, lBundleId);
@@ -64,6 +77,27 @@ public class CheatsheetProcessor {
       break;
     }
   }
+
+  /**
+   * Set the shortcut for opening the cheatsheets
+   * @param pApplication the application
+   * @param lShortcut the shortcut to set
+   */
+  private void setCheatsheetShortcut(MApplication pApplication, String lShortcut) {
+	if (lShortcut != null) {
+    	  List<MBindingTable> lTables = pApplication.getBindingTables();
+    	  for (MBindingTable lBindingTable : lTables) {
+			if (CHEATSHEET_BINDING_TABLE_ID.equals(lBindingTable.getElementId())) {
+				List<MKeyBinding> lBindings = lBindingTable.getBindings();
+				for (MKeyBinding lKeyBinding : lBindings) {
+					if (StickyViewConstants.KEY_BINDING_ID.equals(lKeyBinding.getElementId())) {
+						lKeyBinding.setKeySequence(lShortcut);
+					}
+				}
+			}
+		}
+      }
+	}
 
   /**
    * Get the internationalized cheatsheet
